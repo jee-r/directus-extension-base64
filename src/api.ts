dd1af7b960ee3a7057ff2383d92cd327a48eb3d8
jee <jee@jeer.fr>
2023-03-21 10:35:56 +0100

    feat: add option for width and height

--- src/api.ts
+++ src/api.ts
@@ -4,28 +4,50 @@ const sharp = require("/directus/node_modules/sharp");
 // import { Sharp } from 'sharp';
 
 type Options = {
-	text: string;
+	imageObject?: string,
+	format: string,
+	quality: number,
+	width: number,
+	height: number,
+
 };
 
 export default defineOperationApi<Options>({
 	id: 'base64',
-	handler: async ({ text }, { data, logger, env }) => {
+	handler: async ({ imageObject, format, quality, width }, { data, logger, env }) => {
 
-		const image: any = data.$last.payload
+		// const image: any = imageObject ?? data.$last
+		const image: any = data.$last
+		// logger.info("image: " + JSON.stringify(image))
+		// logger.info("data : " + JSON.stringify(data.$last))
 
-		if (["image/jpeg", "image/png", "image/webp", "image/gif", "image/tiff"].includes(image.type)) {
-			logger.info("file uploaded");
-			logger.info(image.filename_disk);
-			logger.info(image.type);
-			logger.info(env.STORAGE_LOCAL_ROOT);
-			const buffer = await sharp(join(env.STORAGE_LOCAL_ROOT, image.filename_disk)).resize({ width: 10 }).webp({ quality: 10 }).toBuffer();
-			const base64Data: string = Buffer.from(buffer).toString("base64");
-			const base64Prefix: string = "data:image/webp;base64,"
-			const base64: string = base64Prefix + base64Data
-			logger.info(base64);
-			
-			return { base64 };
+		// logger.info("typeof imageObject : " + typeof(imageObject) + ", value: " + imageObject )
+		// logger.info("typeof width : " + typeof(width) + ", width: " + width )
+		// logger.info("typeof quality : " + typeof(quality) + ", value: " + quality )
+		// logger.info("typeof format : " + typeof(format) + ", value: " + format )
+
+		if (!["image/jpeg", "image/png", "image/webp", "image/gif", "image/tiff"].includes(image.type)) {
+			logger.info(`Image type ${image.type} for ${image.filename_disk} is not supported`);
+			return { base64: null };
 		}
+		
 
+
+		const base64_width = width ?? 10
+		const base64_quality = quality ?? 10
+		const base64_format = format ?? 'webp';
+
+		logger.info(`Processing image ${image.filename_disk} with width ${base64_width}, quality ${base64_quality}, and format ${base64_format}`);
+
+		const buffer = await sharp(join(env.STORAGE_LOCAL_ROOT, image.filename_disk)).resize({ width: parseInt(base64_width) }).webp({ quality: parseInt(base64_quality) }).toBuffer();
+		const base64Data: string = Buffer.from(buffer).toString("base64");
+		const base64Prefix: string = "data:image/webp;base64,"
+		const base64: string = base64Prefix + base64Data
+
+		logger.info(`Base64 conversion for ${image.filename_disk} complete : `);
+		logger.info(base64);
+
+
+		return { base64 };
 	},
 });
--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,93 @@ export default defineOperationApp({
 	name: 'base64',
 	icon: 'image',
 	description: 'Generate a base64 on image upload',
-	overview: ({ text }) => [
+	overview: ({ imageObject, format, quality, width, height }) => [
 		{
-			label: 'add possibility to choice width, height and format',
-			text: text,
+			label: 'Directus file object',
+			text: imageObject,
+		},
+		// {
+		// 	label: 'Base64 format',
+		// 	text: format,
+		// },
+		{
+			label: 'Quality',
+			text: quality,
+		},
+		{
+			label: 'Width',
+			text: width,
+		},
+		{
+			label: 'Height',
+			text: height,
 		},
 	],
 	options: [
 		{
-			field: 'text',
-			name: 'Text',
+			field: 'imageObject',
+			name: 'Image Object',
 			type: 'string',
 			meta: {
 				width: 'full',
 				interface: 'input',
 			},
 		},
-	],
+		{
+			field: 'height',
+			name: 'Height',
+			type: 'integer',
+			meta: {
+				width: 'half',
+				interface: 'input',
+			},
+		},
+		{
+			field: 'width',
+			name: 'Width',
+			type: 'integer',
+			meta: {
+				width: 'half',
+				interface: 'input',
+			},
+		},
+		// {
+		// 	field: 'format',
+		// 	name: 'Format',
+		// 	type: 'string',
+		// 	meta: {
+		// 		width: 'half',
+		// 		interface: 'select-dropdown',
+		// 		options: {
+		// 			choices: [
+		// 				{
+		// 					text: 'png',
+		// 					value: 'png',
+		// 				},
+		// 				{
+		// 					text: 'jpg',
+		// 					value: 'jpg',
+		// 				},
+		// 				{
+		// 					text: 'webp',
+		// 					value: 'webp',
+		// 				},
+		// 			],
+		// 			allowOther: true,
+		// 		},
+		// 	},
+		// },
+		{
+			field: 'quality',
+			name: 'Quality',
+			type: 'integer',
+			default_value: 10,
+			meta: {
+				width: 'half',
+				interface: 'input',
+				min: 0,
+				max: 100,
+			},
+		},
+	]
 });
